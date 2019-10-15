49ea8d8fd4727fb6147ec7b9be058e4a7430b492
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-15 11:55:18 -0400

    fixed new user form bug, created index view with links to user pages, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
-        
+        @users = User.all
     end
 
     def new
@@ -8,11 +8,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params(:first_name, :last_name)) 
+        @user.save
+        redirect_to user_path(@user)
     end
 
     def show
-        
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params(first_name, last_name)
+        params.require(:user).permit(:first_name, :last_name)
     end
 
 end
